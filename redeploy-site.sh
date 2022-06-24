68a5ff5f6cc63f63992276797233f776562e2d75
emilyirenelai <88216671+emilyirenelai@users.noreply.github.com>
2022-06-24 00:24:34 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-tmux kill-server
-cd project-22-sum-17-aleena-emily-zareen
+
+tmux kill-session
+
+cd /root/project-22-sum-17-aleena
+
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
+
+source "/root/project-22-sum-17-aleena/python3-virtualenv/bin/activate"
+
+pip install -r requirements.txt
+
+NEW_TMUX="redeploy"
+
+RUN="flask run --host=0.0.0.0"
+
+tmux new-session -d -s "$NEW_TMUX" "$RUN"
